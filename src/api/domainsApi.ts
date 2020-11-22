6ea337eb97f10310ee59a332025f66ea34209e11
LukeDS-it <luca.mcgyver@gmail.com>
2020-11-22 23:00:28 +0100

    fetch domains from firestore

--- src/api/domainsApi.ts
+++ src/api/domainsApi.ts
@@ -6,11 +6,10 @@ const domains = firestore.collection('domains');
 
 export const findAll =
   () => domains
-      .where('creator', '==', auth.currentUser.email)
-      .get()
-      .then((snapshot) => snapshot.docs.map(toDomain))
-
-
+    .where('author', '==', auth.currentUser.email)
+    .where('deleted', '==', false)
+    .get()
+    .then((snapshot) => snapshot.docs.map(toDomain))
 
 const toDomain = (documentData: firebase.firestore.QueryDocumentSnapshot) => {
   return new Domain(documentData.id, documentData.get('title'), documentData.get('icon'))
