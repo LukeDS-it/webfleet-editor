bb82162b3c4615ae9f70f6f3a508d07b1f9a05f3
LukeDS-it <luca.mcgyver@gmail.com>
2020-11-22 21:29:58 +0100

    basic login page

--- src/components/layout/login/LoginView.tsx
+++ src/components/layout/login/LoginView.tsx
@@ -3,6 +3,8 @@ import firebase from 'firebase';
 import {auth} from '../../../utils/firebase';
 import {Redirect, useHistory} from 'react-router';
 import {AuthContext} from '../../../utils/Auth';
+import './LoginView.scss'
+import GLogo from '../../../assets/g-logo.png';
 
 export function LoginView() {
   const history = useHistory();
@@ -20,8 +22,16 @@ export function LoginView() {
   return currentUser
       ? <Redirect to={'/'}/>
       : <div>
-        <button type={'button'} className={'btn btn-login-google'} onClick={handleGoogleLogin}>
-          Login with Google
+        <h1>Welcome to Webfleet!</h1>
+        <p>
+          Please choose how you want to register to our site and start creating
+          your awesome websites!
+        </p>
+        <button type={'button'} className={'login-google'} onClick={handleGoogleLogin}>
+          <span className={'icon'}>
+            <img src={GLogo} alt={'Google logo'}/>
+          </span>
+          <span className={'buttonText'}>Sign in with Google</span>
         </button>
       </div>;
 
