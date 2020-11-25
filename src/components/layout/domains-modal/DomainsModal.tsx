0bd6c708601b4b820a284a1985b19456e0814205
LukeDS-it <luca.mcgyver@gmail.com>
2020-11-25 21:25:59 +0100

    converting icon picker to custom formik field

--- src/components/layout/domains-modal/DomainsModal.tsx
+++ src/components/layout/domains-modal/DomainsModal.tsx
@@ -33,7 +33,7 @@ export function DomainsModal(props: DomainsModalProps) {
           <Form>
             <div className='left'>
               <IconPicker
-                onImageSelect={(img) => p.setFieldValue('icon', img)}
+                fieldName={'icon'}
                 imageIndex={iconsIndex}
                 baseUrl={baseUrl}
                 onOpenPicker={() => setRightVisible(false)}
